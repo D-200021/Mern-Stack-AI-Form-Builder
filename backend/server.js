6128533a3b5d7652afedb61dc21d79230e57a8db
Dhruv Sheth <dhruvsheth@Dhruvs-MacBook-Air.local>
2024-06-17 21:17:04 +0530

    path correction

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import connectToMongoDB from './db/connectToMongoDb.js';
+import connectToMongoDB from './db/connectToMongoDB.js';
 import authRoutes from './routes/auth.routes.js';
 import getData from './routes/getdata.routes.js';
 import path from "path";
 dotenv.config();
 const app = express();
-const Port = process.env.PORT || 3000;
+const Port = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(express.json());
@@ -20,9 +20,6 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/ai", getData);
 
-app.get("/", (req, res) => {
-    res.send("API is running...");
-});
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
